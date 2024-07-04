8a90c3968c495418e04810197d9e447ff23e60f6
Roman Globa <globaroman@gmail.com>
2024-07-04 20:39:17 +0300

    Create the project

---
+++ src/main/java/com/globaroman/testleetcode/TestLeetCodeApplication.java
@@ -0,0 +1,13 @@
+package com.globaroman.testleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLeetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globaroman/testleetcode/TestLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaroman.testleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
